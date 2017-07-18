661cb1fb1400fa5af0a36ecbc8c1ed002f32852e
John Silkey <jpsilkjr86@gmail.com>
2017-07-18 12:46:27 -0700

    added server.js with starter code

---
+++ server.js
@@ -0,0 +1,25 @@
+// dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// set up express app
+const app = express();
+const port = process.env.port || 3000;
+
+// set up body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// routes
+app.get('/', (req, res) => {
+	res.json({test: 'test'});
+});
+
+
+// listens to port
+app.listen(port, () => {
+  console.log('App listening on port ' + port);
+});
