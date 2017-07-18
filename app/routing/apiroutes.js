d859652125039bd729ef6d92cd85e143bc78d883
John Silkey <jpsilkjr86@gmail.com>
2017-07-18 16:46:07 -0700

    added get and post for api/friends uri and tested with test code and Postman

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,8 +1,15 @@
 // treats exports as a function so that this file can serve
 // as an extension of server.js
 module.exports = function(app) {
-	// test route
-	app.get('/test', (req, res) => {
-		res.json({test2: 'test2'});
+	// GET route at api/friends
+	app.get('/api/friends', (req, res) => {
+		res.json({friend: 'friendtest'});
+	});
+
+	// POST route at api/friends
+	app.post('/api/friends', (req, res) => {
+		let newProfile = req.body;
+		console.log(req.body);
+		res.json(newProfile);
 	});
 };
