1944384c582cbc3122fc21db83d4380c5a49f484
John Silkey <jpsilkjr86@gmail.com>
2017-07-18 18:21:32 -0700

    added friends array that is passed between modules and updated with post requests

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,9 +1,9 @@
 // treats exports as a function so that this file can serve
 // as an extension of server.js
-module.exports = function(app) {
+module.exports = function(app, path, friends) {
 	// GET route at api/friends
 	app.get('/api/friends', (req, res) => {
-		res.json({friend: 'friendtest'});
+		res.json({friends: friends});
 	});
 
 	// POST route at api/friends
@@ -11,5 +11,6 @@ module.exports = function(app) {
 		let newProfile = req.body;
 		console.log(req.body);
 		res.json(newProfile);
+		friends.push(newProfile);
 	});
 };
--- server.js
+++ server.js
@@ -13,9 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+// grabs friends from friends.js
+const friends = require('./app/data/friends.js');
+
 // function extensions for routing
-require('./app/routing/htmlroutes.js')(app, path);
-require('./app/routing/apiroutes.js')(app, path);
+require('./app/routing/htmlroutes.js')(app, path, friends);
+require('./app/routing/apiroutes.js')(app, path, friends);
 
 // listens to port
 app.listen(port, () => {
